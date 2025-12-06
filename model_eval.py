dbc161a6c631dcba35749c1ee12ce27e26a57a64
jhasbdmf <oshtyria@uos.de>
2025-12-06 14:58:00 +0100

    feat: eval model on test test

--- model_eval.py
+++ model_eval.py
@@ -2,6 +2,30 @@ import torch
 from architecture import RCNN
 from dataloader_test import ClutteredMNIST, get_mnist_cluttered_loaders
 
+def evaluate(model, loader):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            B = images.size(0)
+          
+
+            logits = model(images)[0]
+            _, preds = logits.max(1)
+            correct += preds.eq(labels).sum().item()
+            total += B
+    return correct / total * 100.0
+
+train_loader, val_loader, test_loader = get_mnist_cluttered_loaders(
+    root="./data", batch_size=256, val_fraction=0.1,
+    image_size=64, n_clutter=50
+)
+
+
+
 # 1. Recreate the model
 model = RCNN()
 # 2. Load checkpoint safely to CPU, regardless of where it was saved
@@ -13,4 +37,6 @@ model.load_state_dict(state_dict)
 # 4. Optionally move model to the available device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-model.eval()
+
+
+print (evaluate(model, test_loader))
